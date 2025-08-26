12f03047a000b1fa3e8434dc8c0dceff2db72635
9raja1999 <draj29209@gmail.com>
2025-08-27 01:39:35 +0500

    [UPDATE] added database share library among services

--- libs/shared/src/modules/database/database.module.ts
+++ libs/shared/src/modules/database/database.module.ts
@@ -14,6 +14,7 @@ import { User, Organization, OrganizationUser } from './entities';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         
+
         entities: [User, Organization, OrganizationUser],
         synchronize: process.env.NODE_ENV !== 'production',
         logging: process.env.NODE_ENV !== 'production',
